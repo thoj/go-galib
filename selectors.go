ccfa40d1aba84777b5843a8721fe52e935e60e16
tj <mail@jager.no>
2009-11-30 23:21:21 +0100

    Start work on Roulette selector

--- selectors.go
+++ selectors.go
@@ -12,6 +12,25 @@ package main
 type GASelector interface {
 	// Select n from pop
 	Select(pop []GAGenome, n int) ([]GAGenome);
+	
 	// String name of selector
 	String() string;
+}
+
+type GARoulettSelector struct {
+	scorefunc func(GAGenome);
+}
+
+func NewRouletteSelector(f func(GAGenome)) (s *GARoulettSelector){
+	s = new(GARoulettSelector);
+	s.scorefunc = f;
+	return s;
+}
+func (s *GARoulettSelector) Select(pop []GAGenome, n int) (g []GAGenome) {
+	//TODO: do something useful here
+	g = make([]GAGenome, 10);
+	return;
+}
+func (s *GARoulettSelector) String() string {
+	return "GARoulettSelector";
 }
