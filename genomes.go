d6e226ad8d0a2415ff5427c63c46383389f5a273
tj <mail@jager.no>
2009-11-30 13:48:25 +0100

    Skeleton for genomes/induvidual

---
+++ genomes.go
@@ -0,0 +1,13 @@
+/*
+Copyright 2009 Thomas Jager <mail@jager.no> All rights reserved.
+Use of this source code is governed by a BSD-style
+license that can be found in the LICENSE file.
+
+go-galib genome
+*/
+
+
+package main
+type GAGenome struct {
+        Gene []GAGene;
+}
