d91225576a7fe4b8956e568a9b3d72c1ee927538
tj <mail@jager.no>
2009-11-30 13:47:39 +0100

    Skeleton for gene definitions

---
+++ genes.go
@@ -0,0 +1,14 @@
+/*
+Copyright 2009 Thomas Jager <mail@jager.no> All rights reserved.
+Use of this source code is governed by a BSD-style
+license that can be found in the LICENSE file.
+
+go-galib gene
+*/
+
+
+package main
+
+type GAGene struct {
+        Value interface{};
+}
