be7b43a71521fad1caf2491bd378d5d8176479ea
tj <mail@jager.no>
2009-11-30 23:20:55 +0100

    Genome for orderd list of int

--- genomes.go
+++ genomes.go
@@ -6,8 +6,40 @@ license that can be found in the LICENSE file.
 go-galib genome
 */
 
-
 package main
-type GAGenome struct {
-        Gene []GAGene;
+
+import (
+	"rand";
+)
+
+type GAGenome interface {
+	//Shuffle genomes araound
+	Shuffle();
+	//Copy a genome;
+	Copy() GAGenome;
+}
+
+//Ordered list genome for problems where the order of Genes matter
+type GAOrderedIntGenome struct {
+	gene []int;
+}
+
+func NewGAOrderedIntGenome(i []int) *GAOrderedIntGenome {
+	g := new(GAOrderedIntGenome);
+	g.gene = i;
+	return g;
+}
+func (g *GAOrderedIntGenome) Shuffle() {
+	l := len(g.gene);
+	for i := 0; i < l; i++ {
+		x := rand.Intn(l);
+		y := rand.Intn(l);
+		g.gene[x], g.gene[y] = g.gene[y], g.gene[x];
+	}
+}
+
+func (g *GAOrderedIntGenome) Copy() *GAOrderedIntGenome {
+	n := new(GAOrderedIntGenome);
+        n.gene = g.gene;
+	return n;
 }
