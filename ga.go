68514c4fe80ccdd7ce69828eb02604333da232e0
tj <mail@jager.no>
2009-11-30 23:20:05 +0100

    Slowly getting more useful

--- ga.go
+++ ga.go
@@ -9,19 +9,28 @@ go-galib gene
 
 package main
 
+import (
+	"fmt";
+)
+
 type GA struct {
 	pop []GAGenome;
 	
-	initializer *GAInitializer;
-	selector *GASelector;
-	mutator *GAMutator;
-	breeder *GABreeder;
+	initializer GAInitializer;
+	selector GASelector;
+	mutator GAMutator;
+	breeder GABreeder;
 }
 
-func NewGA(i *GAInitializer, s *GASelector, m *GAMutator, b *GABreeder) {
+func NewGA(i GAInitializer, s GASelector, m GAMutator, b GABreeder) *GA {
 	ga := new(GA);
 	ga.initializer = i;
 	ga.selector = s;
 	ga.mutator = m;
 	ga.breeder = b;
+	return ga;
+}
+
+func (ga *GA) String() string {
+	return fmt.Sprintf("Initializer = %s, Selector = %s, Mutator = %s Breeder = %s", ga.initializer, ga.selector, ga.mutator, ga.breeder);
 }
