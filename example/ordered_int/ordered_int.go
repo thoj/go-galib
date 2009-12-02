86e71ad52e6286e123885df7d095fafb3fbcbdcc
tj <mail@jager.no>
2009-12-02 03:00:45 +0100

    Trivial

--- example/ordered_int/ordered_int.go
+++ example/ordered_int/ordered_int.go
@@ -8,10 +8,10 @@ import (
 )
 
 // Boring fitness/score function.
-func score (g ga.GAOrderedIntGenome) int {
+func score(g ga.GAOrderedIntGenome) int {
 	var total int;
 	for i, c := range g.Gene {
-		total += c ^ i;
+		total += c ^ i
 	}
 	return total;
 }
@@ -22,18 +22,18 @@ func main() {
 	b := new(ga.GA2PointBreeder);
 
 	s := new(ga.GATournamentSelector);
-	s.Contestants = 4; //Contestants in Tournament
-	s.PElite = 0.3; //Chance of best contestant winning, chance of next is PElite^2 and so on.
-	
+	s.Contestants = 4;	//Contestants in Tournament
+	s.PElite = 0.3;		//Chance of best contestant winning, chance of next is PElite^2 and so on.
+
 	i := new(ga.GARandomInitializer);
 	gao := ga.NewGA(i, s, m, b);
-	gao.PMutate = 0.1; //Chance of mutation
-	gao.PBreed = 0.2; //Chance of breeding
+	gao.PMutate = 0.1;	//Chance of mutation
+	gao.PBreed = 0.2;	//Chance of breeding
 
 	fmt.Printf("%s\n", gao);
-	genome := ga.NewOrderedIntGenome([]int{10,11,12,13,14,15,16,1,2,3,4,5,6,7,8,9,0}, score);
-	gao.Init(200, genome); //Total population
-	
-	gao.Optimize(20); // Run genetic algorithm for 20 generations.
+	genome := ga.NewOrderedIntGenome([]int{10, 11, 12, 13, 14, 15, 16, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0}, score);
+	gao.Init(200, genome);	//Total population
+
+	gao.Optimize(20);	// Run genetic algorithm for 20 generations.
 	gao.PrintTop(10);
 }
