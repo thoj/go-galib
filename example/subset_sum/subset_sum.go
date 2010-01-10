8c740b8f3d4ded129e3b396eda6853f7de7160f9
“Thomas Jager” <mail@jager.no>
2010-01-11 00:31:31 +0100

    Fix comments

--- example/subset_sum/subset_sum.go
+++ example/subset_sum/subset_sum.go
@@ -1,3 +1,10 @@
+/*
+Copyright 2009 Thomas Jager <mail@jager.no> All rights reserved.
+Use of this source code is governed by a BSD-style
+license that can be found in the LICENSE file.
+
+subset sum solver
+*/
 package main
 
 import (
