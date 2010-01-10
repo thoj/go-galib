74bfa72c20541dce6309e7d3cfa12129ec602a1e
“Thomas <mail@jager.no>
2010-01-10 22:45:51 +0100

    Move genomes to directory

--- Makefile
+++ Makefile
@@ -1,6 +1,12 @@
 include $(GOROOT)/src/Make.$(GOARCH)
 
 TARG=ga
-GOFILES=ga.go selectors.go mutators.go initializers.go breeders.go genomes.go
+GOFILES=ga.go \
+	selectors.go \
+	mutators.go \
+	initializers.go \
+	breeders.go \
+	genomes/genomes.go \
+	genomes/ordered_int.go \
 
 include $(GOROOT)/src/Make.pkg 
--- genomes/ordered_int.go
+++ genomes/ordered_int.go
@@ -3,66 +3,18 @@ Copyright 2009 Thomas Jager <mail@jager.no> All rights reserved.
 Use of this source code is governed by a BSD-style
 license that can be found in the LICENSE file.
 
-go-galib genome
+Ordered list genome for problems where the order of Genes matter, tSP for example.
 */
 
 package ga
 
 import (
-	"rand"
 	"fmt"
 	"container/vector"
 	"sort"
+	"rand"
 )
 
-// Genome interface, Not final.
-type GAGenome interface {
-	//Randomize.Genens
-	Randomize()
-	//Copy a genome;
-	Copy() GAGenome
-	//Calculate score
-	Score() int
-	//Reset cached score
-	Reset()
-	//Crossover for this genome
-	Crossover(bi GAGenome, p1, p2 int) (ca GAGenome, cb GAGenome)
-	//Switch for the genome
-	Switch(x, y int)
-	//Splice two genomes;
-	Splice(bi GAGenome, from, to, length int)
-	//Check if genome is valid
-	Valid() bool
-
-	String() string
-	Len() int
-}
-
-type GAGenomes []GAGenome
-
-func (g GAGenomes) Len() int           { return len(g) }
-func (g GAGenomes) Less(i, j int) bool { return g[i].Score() < g[j].Score() }
-func (g GAGenomes) Swap(i, j int)      { g[i], g[j] = g[j], g[i] }
-
-
-func AppendGenomes(slice, data GAGenomes) GAGenomes {
-	l := len(slice)
-	if l+len(data) > cap(slice) {
-		newSlice := make(GAGenomes, (l+len(data))*2)
-		for i, c := range slice {
-			newSlice[i] = c
-		}
-		slice = newSlice
-	}
-	slice = slice[0 : l+len(data)]
-	for i, c := range data {
-		slice[l+i] = c
-	}
-	return slice
-}
-
-
-//Ordered list genome for problems where the order of Genes matter, tSP for example.
 type GAOrderedIntGenome struct {
 	Gene     []int
 	score    int
