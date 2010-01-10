81b8070112149ab45b24434bb6dcc1f90a16f888
“Thomas <mail@jager.no>
2010-01-11 00:27:33 +0100

    Add Fixed bitstring Genome

---
+++ genomes/fixed_bitstring.go
@@ -0,0 +1,96 @@
+/*
+Copyright 2009 Thomas Jager <mail@jager.no> All rights reserved.
+Use of this source code is governed by a BSD-style
+license that can be found in the LICENSE file.
+
+Fixed length Bitstring genome for problems like subset sum
+*/
+
+package ga
+
+import (
+	"fmt"
+	"rand"
+)
+
+type GAFixedBitstringGenome struct {
+	Gene     []bool
+	score    int
+	hasscore bool
+	sfunc    func(ga *GAFixedBitstringGenome) int
+}
+
+func NewFixedBitstringGenome(i []bool, sfunc func(ga *GAFixedBitstringGenome) int) *GAFixedBitstringGenome {
+	g := new(GAFixedBitstringGenome)
+	g.Gene = i
+	g.sfunc = sfunc
+	g.Reset();
+	return g
+}
+
+//Simple 2 point crossover
+func (a *GAFixedBitstringGenome) Crossover(bi GAGenome, p1, p2 int) (GAGenome, GAGenome) {
+	ca := a.Copy().(*GAFixedBitstringGenome)
+	b := bi.(*GAFixedBitstringGenome)
+	cb := b.Copy().(*GAFixedBitstringGenome)
+	copy(ca.Gene[p1:p2+1], b.Gene[p1:p2+1])
+	copy(cb.Gene[p1:p2+1], a.Gene[p1:p2+1])
+	ca.Reset()
+	cb.Reset()
+	return ca, cb
+}
+
+func (a *GAFixedBitstringGenome) Splice(bi GAGenome, from, to, length int) {
+	b := bi.(*GAFixedBitstringGenome)
+	copy(a.Gene[to:length+to], b.Gene[from:length+from])
+	a.Reset()
+}
+
+func (g *GAFixedBitstringGenome) Valid() bool { return true }
+
+func (g *GAFixedBitstringGenome) Switch(x, y int) {
+	g.Gene[x], g.Gene[y] = g.Gene[y], g.Gene[x]
+	g.Reset()
+}
+
+func (g *GAFixedBitstringGenome) Randomize() {
+	l := len(g.Gene)
+	for i := 0; i < l; i++ {
+		x := rand.Intn(2)
+		if x == 1 {
+			g.Gene[i] = true
+		} else {
+			g.Gene[i] = false
+		}
+	}
+	g.Reset()
+}
+
+func (g *GAFixedBitstringGenome) Copy() GAGenome {
+	n := new(GAFixedBitstringGenome)
+	n.Gene = make([]bool, len(g.Gene))
+	for i, c := range g.Gene {
+		n.Gene[i] = c
+	}
+	n.sfunc = g.sfunc
+	n.score = g.score
+	n.hasscore = g.hasscore
+	return n
+}
+
+func (g *GAFixedBitstringGenome) Len() int { return len(g.Gene) }
+
+func (g *GAFixedBitstringGenome) Score() int {
+	if !g.hasscore {
+		g.score = g.sfunc(g)
+		g.hasscore = true
+	}
+	return g.score
+}
+
+func (g *GAFixedBitstringGenome) Reset() { g.hasscore = false }
+
+
+func (g *GAFixedBitstringGenome) String() string {
+	return fmt.Sprintf("%v", g.Gene)
+}
