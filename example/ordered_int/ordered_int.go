19078a0090193a3b8b1bba4035f2369fcf3346ec
“Thomas Jager” <mail@jager.no>
2010-01-11 00:31:00 +0100

    Fix comments

--- example/ordered_int/ordered_int.go
+++ example/ordered_int/ordered_int.go
@@ -1,3 +1,10 @@
+/*
+Copyright 2009 Thomas Jager <mail@jager.no> All rights reserved.
+Use of this source code is governed by a BSD-style
+license that can be found in the LICENSE file.
+
+Example of uing the ordered int genome and mutators
+*/
 package main
 
 import (
